7f1435149360506df831f9e01d56a31ea6620801
Ryan Arana <ryan.arana@gmail.com>
2015-05-11 06:50:28 -0700

    Only change the image in the Glance when the user has fully navigated away and then come back to the Glance

--- We Love You! WatchKit Extension/GlanceController.swift
+++ We Love You! WatchKit Extension/GlanceController.swift
@@ -9,9 +9,6 @@
 import WatchKit
 import Foundation
 
-
-
-
 class GlanceController: WKInterfaceController {
 
     @IBOutlet weak var image: WKInterfaceImage!
@@ -19,35 +16,44 @@ class GlanceController: WKInterfaceController {
 
     var previousUpdate = NSDate(timeIntervalSinceReferenceDate: 0)
     var loveMessage = LoveMessage()
-    let updateInterval = NSTimeInterval(5)
+    let updateInterval = NSTimeInterval(2)
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
+
+        refreshImageIfNeeded()
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-
-        if NSDate().timeIntervalSinceDate(previousUpdate) > updateInterval {
-            loveMessage = LoveMessage()
-            previousUpdate = NSDate()
-        }
-
-        image.setImageNamed(loveMessage.imageName)
-        label.setText(loveMessage.message)
-
-        var userInfo: [NSObject: AnyObject] = [
-            "imageName": loveMessage.imageName,
-            "message": loveMessage.message
-        ]
-        updateUserActivity("message", userInfo: userInfo, webpageURL: nil)
+        updateUI()
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+
+        refreshImageIfNeeded()
+    }
+
+    func updateUI() {
+        image.setImageNamed(loveMessage.imageName)
+        label.setText(loveMessage.message)
+    }
+
+    func refreshImageIfNeeded() {
+        if NSDate().timeIntervalSinceDate(previousUpdate) > updateInterval {
+            loveMessage = LoveMessage()
+            previousUpdate = NSDate()
+            
+            var userInfo: [NSObject: AnyObject] = [
+                "imageName": loveMessage.imageName,
+                "message": loveMessage.message
+            ]
+            updateUserActivity("message", userInfo: userInfo, webpageURL: nil)
+        }
+
+        updateUI()
     }
 }
